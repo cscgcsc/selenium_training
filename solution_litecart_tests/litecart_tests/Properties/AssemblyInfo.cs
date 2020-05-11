c3a155cdaee8566fb5ba5110ccd0911921cdbd12
cscgcsc <cscgcsc>
2020-05-11 23:16:09 +0300

    Exercise 1. The first test: open and stop browser

---
+++ solution_litecart_tests/litecart_tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("litecart_tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("litecart_tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b64c5bd1-2492-4833-a054-f154f50146e6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ solution_litecart_tests/litecart_tests/WorkingWithBrowser.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace litecart_tests
+{
+    [TestFixture]
+    public class WorkingWithBrowser
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Start()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            driver.Url = "http://yandex.ru/";
+            driver.FindElement(By.XPath("//input[@id='text']")).SendKeys("test");
+            driver.FindElement(By.XPath("//button[@type='submit']")).Click();
+        }
+
+        [TearDown]
+        public void Stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
